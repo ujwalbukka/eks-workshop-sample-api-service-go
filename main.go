1dbf91eed0e739f0e0e2617ec6f89b4d0517624f
ujwalbukka <63979260+ujwalbukka@users.noreply.github.com>
2020-06-24 23:35:47 -0400

    Update main.go

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World Version2"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
